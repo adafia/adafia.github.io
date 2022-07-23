ff6b5aaea2270a30b9c6c4e22e0dafe5f175db65
adafia <adafia.samuel@gmail.com>
2022-07-23 14:41:44 +0200

    html structure: about me, interests, projects

---
+++ main.js
@@ -0,0 +1,10 @@
+let experience = document.querySelector('.experience');
+
+const getExperienceInYears = (startDate, dateToCalculate = new Date()) => {
+    const dob = new Date(startDate).getTime();
+    const dateToCompare = new Date(dateToCalculate).getTime();
+    const age = (dateToCompare - dob) / (365 * 24 * 60 * 60 * 1000);
+    return Math.floor(age);
+};
+
+experience.append(getExperienceInYears('01/03/2018'))
